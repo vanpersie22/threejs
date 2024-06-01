40eb36551c50b28616b04435f52521fda82a2319
John Doe <ezealionas@gmail.com>
2024-06-01 08:28:54 -0700

    delete

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/viite-threejs-deployment/", // YOUR REPO NAME HERE
-});
